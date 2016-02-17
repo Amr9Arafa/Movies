6303ddbccdc8cf33a36a60194dc4f6e57ddf0b4c
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-02-17 13:41:19 +0200

    added MovieFragment

--- app/src/main/java/com/example/amrarafa/movies/MainActivity.java
+++ app/src/main/java/com/example/amrarafa/movies/MainActivity.java
@@ -16,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new MovieFragment()).commit();
+        }
     }
 
 
