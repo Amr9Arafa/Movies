96df07665caa613cc4569fb052a373fe8e92e3a8
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-02-18 13:03:25 +0200

    retrieved the parceable object correctly in DetailActivity

--- app/src/main/java/com/example/amrarafa/movies/DetailActivity.java
+++ app/src/main/java/com/example/amrarafa/movies/DetailActivity.java
@@ -1,21 +1,27 @@
 package com.example.amrarafa.movies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
- 
+     
+
+        tv=(TextView)findViewById(R.id.tvForTesting);
+        Bundle data = getIntent().getExtras();
+        MovieDetails movie =(MovieDetails)data.getParcelable("movieDetail");
+        tv.setText(movie.getReleaseDate());
 
 
 
