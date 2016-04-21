093bc9e96d84d6a385a9b40e122f6d546e8a0b27
Amr Mohamed <amr.mohamed.mahmoud.aly@gmail.com>
2016-04-21 13:24:24 +0200

    added sortby option in toolbar

--- app/src/main/java/com/example/amrarafa/movies/MovieFragment.java
+++ app/src/main/java/com/example/amrarafa/movies/MovieFragment.java
@@ -12,6 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -49,7 +52,25 @@ public class MovieFragment extends android.support.v4.app.Fragment implements Lo
         // Add this line in order for this fragment to handle menu events.
         setHasOptionsMenu(true);
     }
-    
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.moive_fragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.movies_type) {
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public void onStart() {
         super.onStart();
